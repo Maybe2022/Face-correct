4ff485248ec824d71c8e35cae5da9c1fcf936e55
Maybe2023 <2101212784@stu.pku.edu.cn>
2023-10-16 16:53:54 +0800

    10_16_16_53

--- test/train.py
+++ test/train.py
@@ -7,8 +7,39 @@
 
 import torch
 import torch.nn as nn
+import numpy as np
+import cv2
 
-data = None
+
+data = np.random.randn((3, 3, 3))
+# how numpy to tensor?
+data = torch.tensor(data, dtype=torch.float32)
+
+# how cv img to tensor
+import cv2
+from torchvision import transforms
+import numpy as np
+
+# 读取图像
+img = cv2.imread('test.jpg')
+
+# 将BGR图像转为RGB图像
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# 定义转换
+transform = transforms.ToTensor()
+
+# 应用转换。注意，如果输入是NumPy ndarray，则它应该是uint8类型，
+# 形状为 (H x W x C)，在0到255的范围内。
+img_tensor = transform(img)
+
+# 如果你想将它添加到批次中或者进行其他的维度变换，可以继续操作这个张量
+# 例如，添加一个批次维度
+img_tensor = img_tensor.unsqueeze(0)
+
+# 现在你有一个适用于模型的PyTorch张量
+
+
 lable = None
 
 
